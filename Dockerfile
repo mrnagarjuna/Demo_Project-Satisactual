b654166c3189ca63fe5e3400eef230615a9539df
Nagarjuna7976 <nagarjuna.intothepro@gmail.com>
2026-03-11 18:41:02 +0530

    commit

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,14 @@ ENV PYTHONUNBUFFERED=1
 
 WORKDIR /app
 
+# Install system dependencies for mysqlclient
+RUN apt-get update && apt-get install -y \
+    gcc \
+    default-libmysqlclient-dev \
+    pkg-config \
+    netcat-openbsd \
+    && rm -rf /var/lib/apt/lists/*
+
 COPY requirements.txt .
 
 RUN pip install --upgrade pip \
--- start.sh
+++ start.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+set -e
 
-python manage.py makemigrations
-python manage.py migrate
+echo "Waiting for MySQL to be ready..."
+
+# Wait until MySQL is ready
+until nc -z -v -w30 mysql_db 3306
+do
+  echo "Waiting 1s..."
+  sleep 1
+done
+
+echo "MySQL started!"
+
+# Run migrations
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files (optional for production)
+# python manage.py collectstatic --noinput
+
+# Start Django server
+echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
